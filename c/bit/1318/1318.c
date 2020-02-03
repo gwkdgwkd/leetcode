1afae58b9c474e2996c3c6cfd728883fe4f804dd
meilike <meilikeyiming@126.com>
2020-02-03 19:10:32 +0800

    add 1318
    Signed-off-by: meilike <meilikeyiming@126.com>

---
+++ c/bit/1318/1318.c
@@ -0,0 +1,15 @@
+int minFlips(int a, int b, int c){
+    int ans = 0;
+    for (int i = 0; i < 31; ++i) {
+        int bit_a = (a >> i) & 1;
+        int bit_b = (b >> i) & 1;
+        int bit_c = (c >> i) & 1;
+        if (bit_c == 0) {
+            ans += bit_a + bit_b;
+        }
+        else {
+            ans += (bit_a + bit_b == 0);
+        }
+    }
+    return ans;
+}
