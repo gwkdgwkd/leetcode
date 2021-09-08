09da72bc109cdeefb9b1b62e03609db9356270e1
lwl <thierryliang@126.com>
2021-09-08 20:00:10 +0800

    add binarytree
    Signed-off-by: lwl <thierryliang@126.com>

---
+++ c/binarytree/traversal/94.二叉树的中序遍历.c
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=c
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+// 递归
+void traversal(struct TreeNode* root, int* returnSize, int* result) {
+  if (root == NULL) {
+    return;
+  }
+  traversal(root->left, returnSize, result);
+  result[(*returnSize)++] = root->val;
+  traversal(root->right, returnSize, result);
+
+  return;
+}
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+  int* result = (int*)malloc(sizeof(int) * 100);
+  *returnSize = 0;
+  traversal(root, returnSize, result);
+
+  return result;
+}
+
+// 迭代
+int idx;
+struct TreeNode* stack[100];
+void push(struct TreeNode* node) { stack[idx++] = node; }
+struct TreeNode* top() {
+  return stack[idx - 1];
+}
+void pop() { --idx; }
+int empty() { return idx == 0 ? 1 : 0; }
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+  memset(stack, 0, sizeof(stack));
+  idx = 0;
+  int* result = (int*)malloc(sizeof(int) * 100);
+  *returnSize = 0;
+
+  if (root == NULL) {
+    return result;
+  }
+
+  struct TreeNode* cur = root;
+  while (cur || !empty()) {
+    if (cur) {
+      push(cur);
+      cur = cur->left;
+    } else {
+      cur = top();
+      pop();
+      result[(*returnSize)++] = cur->val;
+      cur = cur->right;
+    }
+  }
+
+  return result;
+}
+
+// 前中后，统一格式
+int idx;
+struct TreeNode* stack[100];
+void push(struct TreeNode* node) { stack[idx++] = node; }
+struct TreeNode* top() {
+  return stack[idx - 1];
+}
+void pop() { --idx; }
+int empty() { return idx == 0 ? 1 : 0; }
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+  memset(stack, 0, sizeof(stack));
+  idx = 0;
+  int* result = (int*)malloc(sizeof(int) * 100);
+  *returnSize = 0;
+
+  if (root != NULL) {
+    push(root);
+  }
+
+  while (!empty()) {
+    struct TreeNode* node = top();
+    if (node) {
+      pop();              // 将该节点弹出，避免重复操作，下⾯再将右中左节点添加到栈中
+      if (node->right) {  // 添加右节点（空节点不⼊栈）
+        push(node->right);
+      }
+      push(node);  // 添加中节点
+      push(NULL);  // 中节点访问过，但是还没有处理，加⼊空节点做为标记。
+      if (node->left) {  // 添加左节点（空节点不⼊栈）
+        push(node->left);
+      }
+    } else {  // 只有遇到空节点的时候，才将下⼀个节点放进结果集
+      pop();         // 将空节点弹出
+      node = top();  // 新取出栈中元素
+      pop();
+      result[(*returnSize)++] = node->val;  // 加⼊到结果集
+    }
+  }
+
+  return result;
+}
+// @lc code=end
