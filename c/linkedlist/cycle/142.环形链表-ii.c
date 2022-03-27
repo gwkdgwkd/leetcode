02abf4db148952977c903df40521a8b631a9f413
wlliang <thierryliang@126.com>
2022-03-27 19:26:34 +0800

    update
    Signed-off-by: wlliang <thierryliang@126.com>

--- c/linkedlist/cycle/142.环形链表-ii.c
+++ c/linkedlist/cycle/142.环形链表-ii.c
@@ -1,17 +1,37 @@
 /*
- * @lc app=leetcode.cn id=142 lang=c
- *
- * [142] 环形链表 II
- */
+给定一个链表的头节点head，返回链表开始入环的第一个节点。
+如果链表无环，则返回null。
+如果链表中有某个节点，可以通过连续跟踪next指针再次到达，则链表中存在环。
+为了表示给定链表中的环，评测系统内部使用整数pos来表示链表尾连接到链表中的位置（索引从0开始）。
+如果pos是-1，则在该链表中没有环。注意：pos不作为参数进行传递，仅仅是为了标识链表的实际情况。
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+不允许修改链表。
+
+示例1：
+输入：head = [3,2,0,-4], pos = 1
+输出：返回索引为 1 的链表节点
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例2：
+输入：head = [1,2], pos = 0
+输出：返回索引为 0 的链表节点
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例3：
+输入：head = [1], pos = -1
+输出：返回 null
+解释：链表中没有环。
+
+提示：
+链表中节点的数目范围在范围 [0, 104] 内
+-105 <= Node.val <= 105
+pos的值为-1或者链表中的一个有效索引
+*/
+
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
 // 快慢指针
 struct ListNode* detectCycle(struct ListNode* head) {
   struct ListNode* fast = head;
@@ -21,10 +41,12 @@ struct ListNode* detectCycle(struct ListNode* head) {
     fast = fast->next->next;
     // 快慢指针相遇，此时从head和相遇点，同时查找直至相遇
     if (slow == fast) {
-      // 当快慢指针相遇时，让其中任一个指针指向头节点，然后让它俩以相同速度前进，再次相遇时所在的节点位置就是环开始的位置。
+      // 当快慢指针相遇时，让其中任一个指针指向头节点，
+      // 然后让它俩以相同速度前进，再次相遇时所在的节点位置就是环开始的位置。
       struct ListNode* index1 = fast;  // 从相遇节点也出发一个指针
       struct ListNode* index2 = head;  // 从头结点出发一个指针
-      // 这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是环形入口的节点
+      // 这两个指针每次只走一个节点，
+      // 那么当这两个指针相遇的时候就是环形入口的节点
       while (index1 != index2) {
         index1 = index1->next;
         index2 = index2->next;
@@ -34,4 +56,3 @@ struct ListNode* detectCycle(struct ListNode* head) {
   }
   return NULL;
 }
-// @lc code=end
--- c/linkedlist/reverse/206.反转链表.c
+++ c/linkedlist/reverse/206.反转链表.c
@@ -1,19 +1,33 @@
 /*
- * @lc app=leetcode.cn id=206 lang=c
- *
- * [206] 反转链表
- */
+给你单链表的头节点head，请你反转链表，并返回反转后的链表。
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+示例1：
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+示例2：
+输入：head = [1,2]
+输出：[2,1]
+
+示例3：
+输入：head = []
+输出：[]
+
+提示：
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+
+进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+*/
+
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
 
 // 迭代
+// 该算法的实现思想非常直接，就是从当前链表的首元节点开始，一直遍历至链表的最后一个节点，
+// 这期间会逐个改变所遍历到的节点的指针域，另其指向前一个节点
 struct ListNode* reverseList(struct ListNode* head) {
   struct ListNode* cur = head;
   struct ListNode* pre = NULL;
@@ -27,6 +41,44 @@ struct ListNode* reverseList(struct ListNode* head) {
   }
 
   return pre;
+}
+
+// 头插法反转链表
+// 所谓头插法，是指在原有链表的基础上，依次将位于链表头部的节点摘下，
+// 然后采用从头部插入的方式生成一个新链表，则此链表即为原链表的反转版。
+struct ListNode* reverseList(struct ListNode* head) {
+  if (head == NULL || head->next == NULL) {
+    return head;
+  }
+
+  struct ListNode* new_head = NULL;
+  struct ListNode* temp = NULL;
+  while (head) {
+    temp = head;
+    head = head->next;      // 将temp从head中摘除
+    temp->next = new_head;  // 将temp插入到new_head的头部
+    new_head = temp;
+  }
+  return new_head;
+}
+
+// 就地逆置法反转链表
+// 就地逆置法和头插法的实现思想类似，唯一的区别在于，头插法是通过建立一个新链表实现的，
+// 而就地逆置法则是直接对原链表做修改，从而实现将原链表反转。
+struct ListNode* reverseList(struct ListNode* head) {
+  if (head == NULL || head->next == NULL) {
+    return head;
+  }
+  struct ListNode* beg = head;
+  struct ListNode* end = head->next;
+  while (end) {
+    beg->next = end->next;  // 将end从链表中摘除
+    end->next = head;       // 将end移动至链表头
+    head = end;
+    // 调整end的指向，另其指向beg后的一个节点，为反转下一个节点做准备
+    end = beg->next;
+  }
+  return head;
 }
 
 // 递归
@@ -48,4 +100,3 @@ struct ListNode* reverseList(struct ListNode* head) {
   head->next = NULL;
   return last;
 }
-// @lc code=end
