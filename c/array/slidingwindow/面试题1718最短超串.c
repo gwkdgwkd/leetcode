350ecb505dccf191af77a0c9565db42d315f6a3c
lwl <thierryliang@126.com>
2022-05-07 15:22:47 +0800

    update
    Signed-off-by: lwl <thierryliang@126.com>

---
+++ c/array/slidingwindow/面试题1718最短超串.c
@@ -0,0 +1,90 @@
+/*
+假设你有两个数组，一个长一个短，短的元素均不相同。
+找到长数组中包含短数组所有的元素的最短子数组，其出现顺序无关紧要。
+返回最短子数组的左端点和右端点，如有多个满足条件的子数组，返回左端点最小的一个。
+若不存在，返回空数组。
+
+示例1:
+输入:
+big = [7,5,9,0,2,1,3,5,7,9,1,1,5,8,8,9,7]
+small = [1,5,9]
+输出: [7,10]
+
+示例2:
+输入:
+big = [1,2,3]
+small = [4]
+输出: []
+
+提示：
+big.length <= 100000
+1 <= small.length <= 100000
+*/
+
+// 哈希 + 滑动窗口
+struct hashTable {
+  int key;
+  int val;
+  UT_hash_handle hh;
+};
+struct hashTable* hashtable;
+struct hashTable* find(int ikey) {
+  struct hashTable* tmp;
+  HASH_FIND_INT(hashtable, &ikey, tmp);
+  return tmp;
+}
+void insert(int ikey, int ival) {
+  struct hashTable* it = find(ikey);
+  if (it == NULL) {
+    struct hashTable* tmp = malloc(sizeof(struct hashTable));
+    tmp->key = ikey, tmp->val = ival;
+    HASH_ADD_INT(hashtable, key, tmp);
+  } else {
+    it->val = ival;
+  }
+}
+int* shortestSeq(int* big, int bigSize, int* small, int smallSize,
+                 int* returnSize) {
+  hashtable = NULL;
+  for (int i = 0; i < smallSize; ++i) {
+    insert(small[i], 0);
+  }
+  int len = 0;
+
+  int left = 0;
+  int right = 0;
+  int minlen = INT_MAX;
+  int start = 0;
+  while (right < bigSize) {
+    struct hashTable* it1 = find(big[right++]);
+    if (it1 != NULL) {
+      if (it1->val++ == 0) {
+        ++len;
+      }
+    }
+    while (len == smallSize) {
+      if (right - left < minlen) {
+        start = left;
+        minlen = right - left;
+      }
+      struct hashTable* it2 = find(big[left++]);
+      if (it2 != NULL) {
+        if (it2->val-- == 1) {
+          --len;
+        }
+      }
+    }
+  }
+
+  if (minlen == INT_MAX) {
+    *returnSize = 0;
+    return NULL;
+  }
+
+  *returnSize = 2;
+  int* res = (int*)malloc(sizeof(int) * 2);
+  res[0] = start;
+  res[1] = start + minlen - 1;
+
+  return res;
+}
---
+++ c/bit/xor/面试题1719消失的两个数字.c
@@ -0,0 +1,81 @@
+/*
+给定一个数组，包含从1到N所有的整数，但其中缺了两个数字。
+你能在O(N)时间内只用O(1)的空间找到它们吗？
+以任意顺序返回这两个数字均可。
+
+示例1:
+输入: [1]
+输出: [2,3]
+
+示例2:
+输入: [2,3]
+输出: [1,4]
+
+提示：nums.length <= 30000
+*/
+
+// 位运算
+int* missingTwo(int* nums, int numsSize, int* returnSize) {
+  int xor = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    xor ^= ((i + 1) ^ nums[i]);
+  }
+  xor ^= numsSize + 1;
+  xor ^= numsSize + 2;
+
+  int* res = (int*)malloc(sizeof(int) * 2);
+  memset(res, 0, sizeof(int) * 2);
+  *returnSize = 2;
+
+  int mask = xor&(-xor);  // 取最低位的1
+  for (int i = 0; i <= numsSize + 1; ++i) {
+    if (i < numsSize) {
+      if ((nums[i] & mask) == mask) {
+        res[0] ^= nums[i];
+      } else {
+        res[1] ^= nums[i];
+      }
+    }
+    if (((i + 1) & mask) == mask) {
+      res[0] ^= (i + 1);
+    } else {
+      res[1] ^= (i + 1);
+    }
+  }
+
+  return res;
+}
+
+int* missingTwo(int* nums, int numsSize, int* returnSize) {
+  *returnSize = 0;
+  int* res = malloc(sizeof(int) * 2);
+  int* temp = calloc(numsSize + 3, sizeof(int));
+  for (int i = 0; i < numsSize; i++) {
+    temp[nums[i]]++;
+  }
+  for (int i = 1; i < numsSize + 3; i++) {
+    if (temp[i] == 0) res[(*returnSize)++] = i;
+  }
+  return res;
+}
+
+int* missingTwo(int* nums, int numsSize, int* returnSize) {
+  *returnSize = 2;
+  int* res = malloc(sizeof(int) * 2);
+  int sum = 0, two_sum = (1 + numsSize + 2) * (numsSize + 2) / 2;
+  for (int i = 0; i < numsSize; i++) {
+    sum += nums[i];
+  }
+  two_sum = two_sum - sum;
+  int ave = two_sum / 2;
+  int low_sum = 0, high_sum = 0;
+  for (int i = 0; i < numsSize; i++) {
+    if (nums[i] <= ave)
+      low_sum += nums[i];
+    else
+      high_sum += nums[i];
+  }
+  res[0] = (1 + ave) * ave / 2 - low_sum;
+  res[1] = two_sum - res[0];
+  return res;
+}
